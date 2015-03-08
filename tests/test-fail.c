125a10ff01e67e83d59a266e8cee820961b8cdde
zehirmahnn <thibaud.michaud@epita.fr>
2015-03-08 17:03:53 +0100

    add tests which fail in purpose

---
+++ tests/test-fail.c
@@ -0,0 +1,22 @@
+booleen variable_globale;
+void test(void)
+{
+  entier a, b;
+  reel c;
+  a = 0;
+  c = (4.5 + "a");
+  if ((a == b))
+  {
+    b = (a + b);
+  }
+  else
+  {
+    a = (a - b);
+  }
+}
+int main(void)
+{
+  test();
+}
+expression left was of type : real and expression right was of type : string
+In instruction : c = (4.5 + "a");
---
+++ tests/test.c
@@ -0,0 +1,20 @@
+booleen variable_globale;
+void test(void)
+{
+  entier a, b;
+  reel c;
+  a = 0;
+  c = 4.5;
+  if ((a == b))
+  {
+    b = (a + b);
+  }
+  else
+  {
+    a = (a - b);
+  }
+}
+int main(void)
+{
+  test();
+}
