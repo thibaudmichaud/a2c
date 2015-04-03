7dd82942c38b79ba0756bd85980556ede0da8142
zehirmahnn <thibaud.michaud@epita.fr>
2015-04-03 19:06:53 +0200

    remove equal from type_table.h

--- src/type_table.h
+++ src/type_table.h
@@ -12,7 +12,6 @@ struct type_sym
 };
 
 typedef ht_tpl(struct type_sym *) type_table_t;
-int equal(struct type_sym *s1, struct type_sym *s2);
 type_table_t empty_type_table(void);
 void add_var(type_table_t type_table, struct type_sym *sym);
 void del_var(type_table_t type_table, char *ident);
