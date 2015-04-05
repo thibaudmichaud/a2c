972255e8ef978cb1f8dcb1103b5cb2fd6d6641ec
charles <charles.yaiche@epita.fr>
2015-04-05 20:09:24 +0200

    add get_line function

---
+++ src/get_line.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "get_line.h"
+
+char* get_line(FILE *f, int line)
+{
+  char *l = NULL;
+  long pos = ftell(f);
+  size_t len = 0;
+
+  if (f)
+  {
+    rewind(f);
+    for (int i = 0; i < line && (getline(&l, &len, f) != -1); i++)
+      ;
+  }
+
+  fseek(f, pos, SEEK_SET);
+  return l;
+}
+
---
+++ src/get_line.h
@@ -0,0 +1,6 @@
+#ifndef TOOL_BOX_H_
+#define TOOL_BOX_H_
+
+char* get_line(FILE *f, int line);
+
+#endif 
