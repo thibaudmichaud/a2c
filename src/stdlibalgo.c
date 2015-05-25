2cd8547b53e8a122126b26251597f25b7da4c546
Thibaud Michaud <thibaud.michaud@epita.fr>
2015-05-25 18:06:04 +0200

    t_pile: fix implem, finish type checking, add test

--- src/stdlibalgo.c
+++ src/stdlibalgo.c
@@ -33,18 +33,25 @@ void fill_std_types(struct symtable *syms)
   add_type(syms->types, t_char);
 
   struct type *t = malloc(sizeof(struct type));
-  t->name = strdup("t_pile");
+  t->name = strdup("t_node_pile");
   t->type_kind = records_t;
   t->type_val.records_type = malloc(sizeof(struct records));
   list_init(t->type_val.records_type->fields);
   struct field *f = malloc(sizeof(struct field));
   f->type = strdup("entier");
-  f->ident = strdup("data");
+  f->ident = strdup("sommet");
   list_push_back(t->type_val.records_type->fields, f);
   f = malloc(sizeof(struct field));
   f->type = strdup("t_pile");
-  f->ident = strdup("prev");
+  f->ident = strdup("suiv");
   list_push_back(t->type_val.records_type->fields, f);
+  add_type(syms->types, t);
+
+  t = malloc(sizeof(struct type));
+  t->name = strdup("t_pile");
+  t->type_kind = pointer_t;
+  t->type_val.pointer_type = malloc(sizeof(struct pointer));
+  t->type_val.pointer_type->type = strdup("t_node_pile");
   add_type(syms->types, t);
 
   t = malloc(sizeof(struct type));
@@ -100,12 +107,22 @@ void fill_std_fun(struct symtable *syms)
 
 
   f = malloc(sizeof(struct function));
-  f->ident = "vider";
+  f->ident = "vide_pile";
   f->ret = find_type(syms->types, "entier");
   list_init(f->arg);
   arg = malloc(sizeof(struct argument));
   arg->type = find_type(syms->types, "t_pile");
   arg->global = true;
+  list_push_back(f->arg, arg);
+  add_function(syms->functions, f);
+
+  f = malloc(sizeof(struct function));
+  f->ident = "est_vide";
+  f->ret = find_type(syms->types, "booleen");
+  list_init(f->arg);
+  arg = malloc(sizeof(struct argument));
+  arg->type = find_type(syms->types, "t_pile");
+  arg->global = false;
   list_push_back(f->arg, arg);
   add_function(syms->functions, f);
 }
