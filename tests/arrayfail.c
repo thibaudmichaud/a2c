8e8265405251b2626587566406327d34bbb5ed83
Thibaud Michaud <thibaud.michaud@epita.fr>
2015-05-27 15:48:02 +0200

    fix test fail

--- tests/arrayfail.c
+++ tests/arrayfail.c
@@ -1,3 +1,3 @@
-tests/arrayfail.algo:3:14: error: identifier "a" is not an integer
+tests/arrayfail.algo:3:16: error: identifier "a" is not an integer
   t_tab_bool = a * b * 5 booleen
-             ^
+               ^
